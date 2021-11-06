2879c06d81402cbb110bb63cf15d974f2453406b
Solai <solaiyappatce@gmail.com>
2021-11-06 19:40:09 +0530

    colors changed

--- backend.js
+++ backend.js
@@ -2,14 +2,18 @@ var keys = ['1','2','3','4','5','6','7','8','9','0',
             'A','B','C','D','E','F','G','H','I','J',
             'K','L','M','N','O','P','Q','R','S','T',
             'U','V','W','X','Y','Z'];
+var colors = ['cyan','yellow','pink','white'];
+var colorCounter = 0;
+var color = 'cyan';
+var root = document.querySelector(':root');
 var isKeySelected = new Array(36);
 isKeySelected.fill(false);
 document.addEventListener('keydown', playKey);
 function startGame(){
     isGameStarted = true;
     var key = Math.floor(Math.random()*36);
-    document.getElementById("Key"+keys[key]).style.borderColor = 'cyan';
-    document.getElementById("Key"+keys[key]).style.color = 'cyan';
+    document.getElementById("Key"+keys[key]).style.borderColor = getColor();
+    document.getElementById("Key"+keys[key]).style.color = getColor();
     document.getElementById("Key"+keys[key]).style.backgroundColor = 'black';
     isKeySelected[key] = true;
 }
@@ -40,6 +44,17 @@ function normal(){
     for(var i in keys){
         document.getElementById("Key"+keys[i]).style.borderColor = 'black';
         document.getElementById("Key"+keys[i]).style.color = 'black';
-        document.getElementById("Key"+keys[i]).style.backgroundColor = 'cyan';
+        document.getElementById("Key"+keys[i]).style.backgroundColor = getColor();
     }
+}
+
+function changeColor(){
+    colorCounter++;
+    colorCounter = colorCounter%colors.length;
+    color = colors[colorCounter]
+    root.style.setProperty('--primary-color', color);
+}
+
+function getColor(){
+    return color;
 }
